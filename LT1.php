f3e7a7cf85e180e0e17788871da9e1aab86d7a71
SyedAhanaf Chowdhury Auditta <ahanafchowdhury1509@gmail.com>
2025-12-12 02:08:24 +0600

    Labtask1 for php

---
+++ LT1.php
@@ -0,0 +1,22 @@
+<?php
+$length=20;
+$height=30;
+$width=40;
+$area=$length*$height;
+$perimeter=2*($length+$width);
+echo "Rectangle's Area:",$area;
+
+echo "Perimeter",$perimeter;
+
+
+
+
+
+
+
+
+
+
+
+
+?>
