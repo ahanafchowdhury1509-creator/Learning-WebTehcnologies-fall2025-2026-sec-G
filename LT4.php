4aa14d0c944ad1c639eccbc973b1990a40afb3a6
SyedAhanaf Chowdhury Auditta <ahanafchowdhury1509@gmail.com>
2025-12-12 02:26:20 +0600

    Labtask2(4)

---
+++ LT4.php
@@ -0,0 +1,35 @@
+<?php
+$a=10;
+$b=20;
+$c=19;
+if($a>$b){
+    if($a<$c){
+        echo "c is the greatest number";
+    }
+    else{
+        echo "a is the greatest number";
+    }
+}
+
+else{
+    if($b<$c){
+        echo "c is the greatest number";
+    }
+    else{
+        echo "b is the greatest number";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
