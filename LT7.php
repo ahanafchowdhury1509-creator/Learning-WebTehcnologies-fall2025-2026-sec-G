491d2fc75c3193625777ac043f383b77ecb7e543
SyedAhanaf Chowdhury Auditta <ahanafchowdhury1509@gmail.com>
2025-12-12 10:47:29 +0600

    LabTask2(7)

--- LT7.php
+++ LT7.php
@@ -5,8 +5,22 @@ for($i=0;$i<3;$i++){
     }
     echo"<br>";
 }
-
-
+for($i=3;$i>0;$i--){
+    for($j=0;$j<$i;$j++){
+        echo $j+1;
+    }
+    echo"<br>";
+}
+$A=['A','B','C','D','E','F'];
+$k=0;
+for($i=0;$i<3;$i++){
+    for($j=0;$j<=$i;$j++){
+        
+        echo $A[$k];
+        $k++;
+    }
+    echo "<br>";
+}
 
 
 
