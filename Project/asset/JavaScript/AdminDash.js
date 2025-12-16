a3df792d5e3a9efea1b9e30c8fc5641f1706191b
SyedAhanaf Chowdhury Auditta <ahanafchowdhury1509@gmail.com>
2025-12-16 22:07:18 +0600

    AdminDash Board JavaScript

---
+++ Project/asset/JavaScript/AdminDash.js
@@ -0,0 +1,16 @@
+const MOCK_METRICS = {
+    totalDonors: 1250,
+    activeRequests: 45,
+    emergencyRequests: 3,
+    scheduledEvents: 7,
+    pendingApprovals: 8,
+    criticalAlerts: 2,
+};
+
+const MOCK_ANALYTICS = {
+    demandDistribution: { 'O+': 80, 'A+': 55, 'B+': 40, 'O-': 25, 'AB+': 15, 'A-': 5 },
+    fulfillmentRate: 88.5,
+    avgResponseTime: "12 min 30 sec",
+    emergencyCount: 3,
+    normalCount: 42
+};
